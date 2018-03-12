7a22f2d79b5a584742a6fc5c0dd9ceccd3af3128
ViYaYaYa <eureka-studio@qq.com>
2018-03-12 14:41:58 +0800

    我看我还是把图片放样式里面吧唉

--- src/vue/empty.vue
+++ src/vue/empty.vue
@@ -1,6 +1,6 @@
 <template>
   <aside class="bmui-empty">
-    <img class="bmui-empty-img" src="~src/assets/empty_page.png">
+    <i class="bmui-empty-img"/>
     <p class="bmui-empty-text">暂无数据</p>
   </aside>
 </template>
